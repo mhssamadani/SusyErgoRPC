a24c7fb7f9518ef1d97b7cbea56f8dc6f6bccc21
HFazelinia <hadifazelinia78@gmail.com>
2021-12-20 03:07:04 +0300

    added json rpc

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,37 @@
 import Contracts from "./susy/boxes";
-Contracts.generateSponsorContract().then(res=>console.log(res));
+//Contracts.generateSponsorContract().then(res=>console.log(res));
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const { JSONRPCServer } = require("json-rpc-2.0");
+
+const server = new JSONRPCServer();
+
+// First parameter is a method name.
+// Second parameter is a method itself.
+// A method takes JSON-RPC params and returns a result.
+// It can also return a promise of the result.
+server.addMethod("vaa", (args: { bytes: Uint8Array }) => {
+  console.log("vaa called with js: ", args.bytes)
+})
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/json-rpc", (req: any, res: any) => {
+  const jsonRPCRequest = req.body;
+  // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
+  // Alternatively, you can use server.receiveJSON, which takes JSON string as is (in this case req.body).
+  server.receive(jsonRPCRequest).then((jsonRPCResponse: any) => {
+    if (jsonRPCResponse) {
+      res.json(jsonRPCResponse);
+    } else {
+      // If response is absent, it was a JSON-RPC notification method.
+      // Respond with no content status (204).
+      res.sendStatus(204);
+    }
+  });
+});
+
+app.listen(8080);
+
