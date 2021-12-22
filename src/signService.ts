a906e00142dd57fcdf18303a6c3bd9a4dccab0d5
HFazelinia <hadifazelinia78@gmail.com>
2021-12-23 01:06:37 +0300

    signMsg method is complete

--- src/signService.ts
+++ src/signService.ts
@@ -5,7 +5,7 @@ import { TextEncoder } from "util";
 
 var ecurve = require('ecurve')
 var BigInteger = require('bigi')
-const blake2 = require("blake2")
+const { blake2b } = require("ethereum-cryptography/blake2b")
 var secureRandom = require('secure-random')
 
 function rand() {
@@ -19,21 +19,12 @@ function checkSign(box: any): boolean {
     return false
 }
 
-function blake2bHash(msg: Uint8Array) {
-    var h = blake2.createHash('blake2b', {digestLength: 32})
-    h.update(msg)
-    return h.digest("hex")
-}
-
 function signMsg(msg: Uint8Array, sk: string) {
     while (true) {
-        //let r = rand()
-        let r = BigInteger.fromHex("c7065537c8a4473c24f66efef51cdd7e07f0c767db10ae20a3df025bcb551753")
+        let r = rand()
         let ecparams = ecurve.getCurveByName('secp256k1')
         let a = ecparams.G.multiply(r)
-        let msgHash = blake2bHash(msg)
-        console.log(`msgHash: ${msgHash}`)
-        console.log(`msgHash bigint: ${BigInteger.fromHex(msgHash).toString(16)}`)
+        let msgHash = blake2b(msg, 32).toString('hex') // TODO: Result is different from scala Blake2b256 hash
         let z: any = r.add(BigInteger.fromHex(sk).multiply(BigInteger.fromHex(msgHash))).remainder(ecparams.n)
         if (z.bitCount() < 256) {
             return (a.getEncoded().toString('hex'), z.toString(16))
@@ -42,14 +33,6 @@ function signMsg(msg: Uint8Array, sk: string) {
 }
 
 console.log("[*] sign service started...")
-
-// TODO: remove this (this is for test) =====================
-var enc = new TextEncoder()
-let msg = enc.encode("s".repeat(50))
-let secret = "17b42abec839188f816f2b0c39be2a401bb05a0a152db37e87f76bb5ae38f6db"
-console.log(signMsg(msg, secret))
-// ==========================================================
-
 
 // loop this procedure (e.g. once in 3 minutes)
 
