d8122d1a8ef22806e798bd93040f916b79a498e0
sahandzou <sahandzoufan79@gmail.com>
2021-12-23 13:41:04 +0330

    createRequest

--- src/susy/boxes.ts
+++ src/susy/boxes.ts
@@ -1,8 +1,11 @@
 import Contracts from "./contracts";
+import config from "../config/conf.json";
+import {I64} from "ergo-lib-wasm-nodejs";
 
 const ergoLib = require("ergo-lib-wasm-nodejs");
 
 export class Boxes {
+    // TODO: should checked I64 or number is ok
     static async getSponsor(value: number) {
         const sponsorValue = ergoLib.BoxValue.from_i64(ergoLib.I64.from_str(value.toString()));
         return new ergoLib.ErgoBoxCandidateBuilder(
@@ -11,5 +14,29 @@ export class Boxes {
             0
         )
             .build();
+    }
+
+    static async getBank(tokenCount:I64){
+        const value=ergoLib.BoxValue.from_i64(ergoLib.I64.from_str("1000000000"));
+        const bankBuilder= new ergoLib.ErgoBoxCandidateBuilder(
+            value,
+            await Contracts.generateBankContract(),
+            0
+        );
+        bankBuilder.add_token(
+            ergoLib.token.from_str(config.token.bankNFT),
+            ergoLib.token.TokenAmount.from_i64(
+                ergoLib.I64.from_str(
+                    "1"
+                )
+            )
+        );
+        bankBuilder.add_token(
+            ergoLib.token.from_str(config.token.bankToken),
+            ergoLib.token.TokenAmount.from_i64(
+            tokenCount
+            )
+        );
+        return bankBuilder.build();
     }
 }
--- src/susy/transaction.ts
+++ src/susy/transaction.ts
@@ -40,9 +40,7 @@ async function updateVAABox(
             )
         )
     );
-    for (let i = 0; i < 3; i++) {
-        VAABuilder.set_register_value(i, VAABox.register_value(i)!);
-    }
+    for (let i = 0; i < 3; i++) VAABuilder.set_register_value(i, VAABox.register_value(i)!);
     VAABuilder.set_register_value(3,
         ergoLib.Constant.from_i32_array(
             [Math.pow(2, index), checksum, (signatureCount + 1), index]
@@ -63,10 +61,26 @@ async function updateVAABox(
     inputBoxes.add(VAABox);
     inputBoxes.add(sponsor);
     inputBoxes.add(guardianBox);
+
     const boxSelection = new ergoLib.BoxSelection(inputBoxes, new ergoLib.ErgoBoxAssetsDataList());
-    const txOutput = new ergoLib.ErgoBoxCandidates(outWormhole);
-    txOutput.add(outVAA);
-    txOutput.add(outSponsor);
+    const tx=generateTx(inputBoxes,[outWormhole,outVAA,outSponsor],sponsor);
+
+    tx.set_data_inputs(ergoLib.DataInput(guardianBox.box_id()));
+    // TODO:should check signer secret key
+    const sks = new ergoLib.SecretKeys();
+    sks.add(ergoLib.SecretKey.dlog_from_bytes(hexStringToByte(ergoLib.Address.config.updateSK)));
+    const wallet = ergoLib.Wallet.from_secrets(sks);
+    const tx_data_inputs = new ergoLib.ErgoBoxes(guardianBox);
+    const ctx = await ApiNetwork.getErgoStateContexet();
+    const signedTx = wallet.sign_transaction(ctx, tx, inputBoxes, tx_data_inputs)
+    return signedTx.to_json();
+
+}
+
+function generateTx(inputBoxes:any,outputs:[any,...any[]],sponsor:any){
+    const boxSelection = new ergoLib.BoxSelection(inputBoxes, new ergoLib.ErgoBoxAssetsDataList());
+    const txOutput = new ergoLib.ErgoBoxCandidates(outputs[0]);
+    for(let i=1;i<outputs.length;i++) txOutput.add(outputs[i]);
     const tx = ergoLib.TxBuilder.new(
         boxSelection,
         txOutput,
@@ -79,6 +93,42 @@ async function updateVAABox(
         ergoLib.Address.recreate_from_ergo_tree(sponsor.ergo_tree()),
         ergoLib.BoxValue.SAFE_USER_MIN()
     );
+    return tx;
+}
+
+export async function createPayment(bank: ErgoBox, VAABox: ErgoBox, sponsor: ErgoBox, guardianBox: ErgoBox) {
+    // TODO:is register value index same as sacala version?
+    if (VAABox.register_value(3)?.to_i32_array()[1]! < config.bftSignatureCount) {
+        throw("Not enough signature");
+    }
+    const payload = VAABox.register_value(0)?.to_coll_coll_byte()[1]!;
+    const amount = new DataView(payload.slice(1, 33)).getInt32(0, false);
+    const tokenId = payload.slice(33, 65);
+    const userAddress = payload.slice(67, 103);
+    const fee = new DataView(payload.slice(105, 137)).getInt32(0, false);
+    const outBank = await Boxes.getBank(
+        bank.tokens().get(1).amount().as_i64().checked_add(
+            ergoLib.I64.from_str((amount - fee).toString())
+        )
+    );
+    const VAABuilder = new ergoLib.ErgoBoxCandidateBuilder(
+        VAABox.value(),
+        await Contracts.generateVAAContract(),
+        0
+    );
+    VAABuilder.add_token(
+        VAABox.tokens().get(0).id(),
+        VAABox.tokens().get(0).amount()
+    );
+
+    for (let i = 0; i < 3; i++) VAABuilder.set_register_value(i, VAABox.register_value(i)!);
+    const outVAA = VAABuilder.build();
+    const outSponsor = await Boxes.getSponsor(sponsor.value().as_i64().as_num() - config.fee * 2);
+    const inputBoxes = ergoLib.ErgoBoxes.new(bank);
+    inputBoxes.add(VAABox);
+    inputBoxes.add(sponsor);
+    const tx=generateTx(inputBoxes,[bank,outVAA,outSponsor],sponsor);
+    tx.set_data_inputs(ergoLib.DataInput(guardianBox.box_id()));
     // TODO:should check signer secret key
     const sks = new ergoLib.SecretKeys();
     sks.add(ergoLib.SecretKey.dlog_from_bytes(hexStringToByte(ergoLib.Address.config.updateSK)));
