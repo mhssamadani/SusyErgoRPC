5d5b249701771c84e47190aae633383f0736a5ec
HFazelinia <hadifazelinia78@gmail.com>
2022-01-04 06:02:46 +0300

    changed import of ergoLib to * as wasm

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import config from "../config/conf";
 import Contracts from "../susy/contracts";
-import ergoLib from "ergo-lib-wasm-nodejs"
+import * as wasm from "ergo-lib-wasm-nodejs"
 import { GuardianBox, VAABox } from "../models/boxes";
 import { ergoTreeToAddress } from "../utils/codec";
 import { TX } from "../models/types"
@@ -44,9 +44,9 @@ class ApiNetwork {
     // TODO: should checked with new function
     static getErgoStateContext = async () => {
         const blockHeaderJson = await this.getLastBlockHeader();
-        const blockHeaders = ergoLib.BlockHeaders.from_json(blockHeaderJson);
-        const preHeader = ergoLib.PreHeader.from_block_header(blockHeaders.get(0));
-        return new ergoLib.ErgoStateContext(preHeader, blockHeaders);
+        const blockHeaders = wasm.BlockHeaders.from_json(blockHeaderJson);
+        const preHeader = wasm.PreHeader.from_block_header(blockHeaders.get(0));
+        return new wasm.ErgoStateContext(preHeader, blockHeaders);
     }
 
     static getBoxWithToken = (token: string) => {
@@ -106,21 +106,21 @@ class ApiNetwork {
         return boxes.boxes.map(box => new VAABox(box));
     }
 
-    static getWormholeBox = async (): Promise<ergoLib.ErgoBox> => {
-        const box: ergoLib.ErgoBox = ergoLib.ErgoBox.from_json(await ApiNetwork.getBoxWithToken(config.token.wormholeNFT).then(box => box.data.items[0]))
+    static getWormholeBox = async (): Promise<wasm.ErgoBox> => {
+        const box: wasm.ErgoBox = wasm.ErgoBox.from_json(await ApiNetwork.getBoxWithToken(config.token.wormholeNFT).then(box => box.data.items[0]))
         return await ApiNetwork.trackMemPool(box)
     }
 
-    static getBankBox = async (token: string, amount: number | string): Promise<ergoLib.ErgoBox> => {
+    static getBankBox = async (token: string, amount: number | string): Promise<wasm.ErgoBox> => {
         const bankBoxes = await ApiNetwork.getBoxWithToken(config.token.bankNFT).then(res => res.data.items)
-        return bankBoxes.map((item: any) => ergoLib.ErgoBox.from_json(JSON.stringify(item))).filter((box: ergoLib.ErgoBox) => {
+        return bankBoxes.map((item: any) => wasm.ErgoBox.from_json(JSON.stringify(item))).filter((box: wasm.ErgoBox) => {
             return (box.tokens().get(1).id().to_str() === token && box.tokens().get(1).amount().as_i64().as_num() > Number(amount))
         })[0]
     }
 
-    static getSponsorBox = async (): Promise<ergoLib.ErgoBox> => {
-        const address = ergoLib.Address.recreate_from_ergo_tree((await Contracts.generateSponsorContract()).ergo_tree()).to_base58(config.networkType)
-        const box: ergoLib.ErgoBox = ergoLib.ErgoBox.from_json(await ApiNetwork.getBoxesByAddress(address).then(box => box.data.items[0]))
+    static getSponsorBox = async (): Promise<wasm.ErgoBox> => {
+        const address = wasm.Address.recreate_from_ergo_tree((await Contracts.generateSponsorContract()).ergo_tree()).to_base58(config.networkType)
+        const box: wasm.ErgoBox = wasm.ErgoBox.from_json(await ApiNetwork.getBoxesByAddress(address).then(box => box.data.items[0]))
         return await ApiNetwork.trackMemPool(box)
     }
 
@@ -128,15 +128,15 @@ class ApiNetwork {
         return await explorerApi.get(`/api/v1/transactions/${txId}`).then(res => res.data)
     }
 
-    static trackMemPool = async (box: ergoLib.ErgoBox): Promise<any> => {
+    static trackMemPool = async (box: wasm.ErgoBox): Promise<any> => {
         const address: string = ergoTreeToAddress(box.ergo_tree())
-        let mempoolBoxesMap = new Map<string, ergoLib.ErgoBox>();
+        let mempoolBoxesMap = new Map<string, wasm.ErgoBox>();
         (await ApiNetwork.getBoxesByAddress(address).then(res => res.data.items)).forEach((tx: TX) => {
             for (var inBox of tx.inputs) {
                 if (inBox.address) {
                     for (var outBox of tx.outputs) {
                         if (outBox.address) {
-                            mempoolBoxesMap.set(inBox.boxId, ergoLib.ErgoBox.from_json(JSON.stringify(outBox)))
+                            mempoolBoxesMap.set(inBox.boxId, wasm.ErgoBox.from_json(JSON.stringify(outBox)))
                             break
                         }
                     }
@@ -144,7 +144,7 @@ class ApiNetwork {
                 }
             }
         })
-        var lastBox: ergoLib.ErgoBox = box
+        var lastBox: wasm.ErgoBox = box
         while (mempoolBoxesMap.has(lastBox.box_id().to_str())) lastBox = mempoolBoxesMap.get(lastBox.box_id().to_str())!
         return lastBox
     }
